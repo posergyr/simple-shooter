21142665fd212a6def44790398e9d743d34c54d4
Sergey Potapov <posergyr@gmail.com>
2022-08-19 18:00:55 +0300

    Reworked Movement Input

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,38 +1,1 @@
-using UnityEngine;
-using UnityEngine.InputSystem;
-
-public class Movement : MonoBehaviour
-{
-    private Rigidbody2D _rb;
-
-    private Vector2 _mMove;
-    private Vector2 _mFire;
-    private Vector2 _currentInputVector;
-    private Vector2 _currentInputVelocity;
-    
-    [SerializeField] private float speed = 200.00f;
-    [SerializeField] private float smoothInputSpeed = 0.15f;
-    
-    
-    private void Start()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void OnMove(InputValue value)
-    { 
-        _mMove = value.Get<Vector2>();
-    }
-    
-    private void OnFire(InputValue value)
-    {
-        Debug.Log("Fire!");
-    }
-
-    private void FixedUpdate()
-    {
-        _currentInputVector = Vector2.SmoothDamp(_currentInputVector, _mMove, ref _currentInputVelocity, smoothInputSpeed);
-        var smoothMovement = new Vector2(_currentInputVector.x, _currentInputVector.y); 
-        _rb.velocity = smoothMovement * (speed * Time.deltaTime);
-    }
-}
+using System;using UnityEngine;using UnityEngine.InputSystem;public class Movement : MonoBehaviour{    private Rigidbody2D _rb;    private InputBindings _inputBindings;    private Vector2 _movement;    private bool _movementPressed;    private bool _fire;        private Vector2 _currentInputVector;    private Vector2 _currentInputVelocity;    [SerializeField] private float speed = 200.00f;    [SerializeField] private float smoothInputSpeed = 0.15f;    private void Awake()    {        _inputBindings = new InputBindings();        _inputBindings.Player.Movement.started += OnMovementInput;        _inputBindings.Player.Movement.canceled += OnMovementInput;        _inputBindings.Player.Movement.performed += OnMovementInput;    }    private void OnMovementInput(InputAction.CallbackContext ctx)    {        _movement = ctx.ReadValue<Vector2>();    }        private void Start()    {        _rb = GetComponent<Rigidbody2D>();    }    private void OnEnable()    {        _inputBindings.Player.Enable();    }    private void OnDisable()    {        _inputBindings.Player.Disable();    }        private void FixedUpdate()    {        _currentInputVector = Vector2.SmoothDamp(_currentInputVector, _movement, ref _currentInputVelocity, smoothInputSpeed);        var smoothMovement = new Vector2(_currentInputVector.x, _currentInputVector.y);         _rb.velocity = smoothMovement * (speed * Time.deltaTime);    }}
